89dc980adda9713c01e23d27934625ddc64d0e6b
amritayan banerjee <amritayanbanerjee@gofynd.com>
2024-12-21 10:50:50 +0530

    server set up

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const app = require("./app");
+const { connectDB, sequelize } = require("./utils/db");
+
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+  await connectDB();
+  await sequelize.sync({ alter: true }); // Sync models to the database
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})();
